4b8886dd4aeb7c875a5100a4734b35a8c7b34832
snakeaid <illia.antypenko@gmail.com>
2022-09-15 15:36:54 +0300

    Add manual deploy config

--- SchoolJournal.API/Program.cs
+++ SchoolJournal.API/Program.cs
@@ -30,10 +30,10 @@ builder.Services.AddMassTransit(x =>
         var username = Environment.GetEnvironmentVariable("RABBIT_USERNAME");
         var password = Environment.GetEnvironmentVariable("RABBIT_PASSWORD");
 
-        cfg.Host(url, port, vhost, h =>
+        cfg.Host("amqps://yudntuee:h8_qUy7Uwean7FnlwBrZWah8399WnbO2@shark.rmq.cloudamqp.com", 5672, "yudntuee", h =>
         {
-            h.Username(username);
-            h.Password(password);
+            h.Username("yudntuee");
+            h.Password("h8_qUy7Uwean7FnlwBrZWah8399WnbO2");
         });
         // }
 
